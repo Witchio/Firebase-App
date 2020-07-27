4ffd60492d9ae00e96dbe78845cd187edf093e2b
Witchio <65949225+Witchio@users.noreply.github.com>
2020-07-27 08:01:46 +0200

    Commit test

---
+++ script.js
@@ -0,0 +1,6 @@
+let csvToJson = require('convert-csv-to-json');
+
+let fileInputName = 'test-data.csv';
+let fileOutputName = 'test-data.json';
+
+csvToJson.generateJsonFileFromCsv(test-data.csv, test-data.json);

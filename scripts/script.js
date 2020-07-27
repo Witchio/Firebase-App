390cc8aad4b201eb05d9e0bbdefdbd59f9570058
Witchio <65949225+Witchio@users.noreply.github.com>
2020-07-27 18:44:29 +0200

    Catching object from Firebase
    Getting full objects from firebase using js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// Your web app's Firebase configuration
+// Web app's basic configuration, connect to firebase db
 var firebaseConfig = {
     apiKey: "AIzaSyBfshrbgEZppCu-OhYvMdP9Bt662XDGW14",
     authDomain: "ng-technical-eval.firebaseapp.com",
@@ -12,14 +12,21 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-//var database = firebase.database();
-const preObject = document.getElementById('ng-technical-eval');
+
+
 
 //Create references
 const dbRefObject = firebase.database().ref().child('1');
 
-//Sync object changes
-dbRefObject.on('value', snap => console.log(snap.val()));
+//Sync object changes and test with console log if data is connected to js
+// Creating an array containing all the objects needed
+var dataArray = [];
+let data = dbRefObject.once('value').then(function (snapshot) {
+    dataToPush = snapshot.val();
+    dataArray.push(dataToPush);
+})
+
+console.log(dash);
 
 
 
@@ -27,4 +34,5 @@ dbRefObject.on('value', snap => console.log(snap.val()));
 /**
  * * Sources
  * ? https://www.youtube.com/watch?v=noB98K6A0TY
+ * ? https://firebase.google.com/docs/database/web/read-and-write
  */
